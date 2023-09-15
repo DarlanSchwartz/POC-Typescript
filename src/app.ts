e186f276e72cb2f1907300884c1b05baa1f2c79e
Schwartz <darlanswtz@gmail.com>
2023-09-15 20:27:44 -0300

    Fix: error handler use

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express, { Application, json } from "express";
 import "express-async-errors";
 import cors from 'cors';
 import postsRouter from "./routes/posts.routes";
+import errorHandler from "middlewares/errorHandler.middleware";
 
 const app : Application = express();
 
 app.use(cors());
 app.use(json());
+app.use(errorHandler);
 app.use(postsRouter);
 const PORT = process.env.PORT || 5000;
 
