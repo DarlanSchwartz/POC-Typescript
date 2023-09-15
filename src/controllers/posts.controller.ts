e8c4280b4c13dd5f1c7f56ffd1d80110844a2f73
Schwartz <darlanswtz@gmail.com>
2023-09-15 16:26:40 -0300

    Feat: everything done, needs testing now

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import PostService from "../services/post.services.ts";
 import { Post } from "protocols/post.types.ts";
-import ErrorType from "protocols/error.types.ts";
 
 async function getAll(req: Request, res: Response) {
   const posts = await PostService.getAll();
@@ -16,7 +15,6 @@ async function create(req: Request, res: Response) {
 
 async function like(req: Request, res: Response) {
   const id = Number(req.params.id);
-  if(!id) throw {type : ErrorType.BAD_REQUEST, message:`you need to pass an id to like a post - id passed ${id}` };
   await PostService.like(id);
   return res.sendStatus(201);
 }
@@ -24,14 +22,12 @@ async function like(req: Request, res: Response) {
 async function edit(req: Request, res: Response) {
   const id = Number(req.params.id);
   const newPost : Post = req.body;
-  if(!id) throw {type : ErrorType.BAD_REQUEST, message:`you need to pass an id to edit a post - id passed ${id}` };
   await PostService.update(id,newPost);
   return res.sendStatus(204);
 }
 
 async function remove(req: Request, res: Response) {
   const id = Number(req.params.id);
-  if(!id) throw {type : ErrorType.BAD_REQUEST, message:`you need to pass an id to remove a post - id passed ${id}` };
   await PostService.remove(id);
   return res.sendStatus(204);
 }
--- src/services/post.services.ts
+++ src/services/post.services.ts
@@ -1,24 +1,29 @@
+import ErrorType from "protocols/error.types";
 import { Post } from "../protocols/post.types";
+import PostRepository from "repositories/post.repositories";
 
 async function getAll(): Promise<Array<Post>> {
-  const posts = null;
+  const posts = await PostRepository.getAll();
   return posts;
 }
 
 async function create(post: Post): Promise<void> {
-
+  await PostRepository.create(post);
 }
 
 async function like(postId: number): Promise<void> {
-
+  if(!postId) throw {type : ErrorType.BAD_REQUEST, message:`you need to pass an id to like a post - id passed ${postId}` };
+  await PostRepository.like(postId);
 }
 
 async function update(postId: number, newPost : Post): Promise<void> {
-
+  if(!postId) throw {type : ErrorType.BAD_REQUEST, message:`you need to pass an id to edit a post - id passed ${postId}` };
+  await PostRepository.edit(newPost,postId);
 }
 
 async function remove(postId: number): Promise<void> {
-
+  if(!postId) throw {type : ErrorType.BAD_REQUEST, message:`you need to pass an id to remove a post - id passed ${postId}` };
+  await PostRepository.remove(postId);
 }
 
 
